18dd5fd5e79208e78c9c4d0987eb2808df1897a7
Max Rosenstein <mrosenstein99@gmail.com>
2017-10-30 14:51:34 +0200

    added logging

--- genepisodepage.sh
+++ genepisodepage.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo "Generating article..."
 python hirssparser.py $1
+echo "Editing wiki..."
 cat formatted.txt | python editpage.py $2
 rm formatted.txt
+echo "Process Complete.\n"
